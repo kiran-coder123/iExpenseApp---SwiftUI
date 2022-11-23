8e78ec6b49bed069a9dba3687ef3c0282b15033f
kiran sonne <kiransonne04@gmail.com>
2022-11-23 13:06:52 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Kiran Sonne on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
